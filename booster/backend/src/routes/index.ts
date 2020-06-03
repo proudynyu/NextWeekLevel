446c3169b9b793cd7dda830ef77f0a57fa3fd24c
Igor Becker <igor_nyu@hotmail.com>
2020-06-03 09:48:48 -0300

    :sparkles: backend initial with routes

--- booster/backend/src/routes/index.ts
+++ booster/backend/src/routes/index.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
+import PointsControllers from '../controllers/pointsControllers';
+import ItensControllers from '../controllers/itensControllers';
 
 const routes = Router();
 
+const itensControllers = new ItensControllers;
+const pointsControllers = new PointsControllers;
+
+routes.get('/itens', itensControllers.index);
+
+routes.get('/points', pointsControllers.index);
+routes.post('/points', pointsControllers.create);
+routes.get('/points/:id', pointsControllers.show)
+
 routes.get('/', (req, res) => {
-    return res.json({
-        msg: 'Hello World',
-    })
-})
+    return res.json({msg: 'Hello World'});
+});
 
 export default routes;
