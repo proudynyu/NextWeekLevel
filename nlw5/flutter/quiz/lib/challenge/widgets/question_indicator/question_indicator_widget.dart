0dafa6f4dbf6c1fa1db0b77f722fd2953a792bcf
Igor Becker <igor_nyu@hotmail.com>
2021-04-26 21:35:07 -0300

    widgets

---
+++ nlw5/flutter/quiz/lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          
+        ]
+      ),
+    );
+  }
+}
